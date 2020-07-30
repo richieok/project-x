fe722344c2c5898580adc279cd7220f2b0e4b83e
richieok <richie.okoro@gmail.com>
2020-07-30 12:08:29 +0100

    minimal

--- src/routes/shop/index.svelte
+++ src/routes/shop/index.svelte
@@ -1,12 +1,13 @@
-<svelte:head>
-    <title>Shop</title>
-</svelte:head>
 
 <style>
     div {
         margin: 1rem; 
     }
 </style>
+
+<svelte:head>
+    <title>Shop</title>
+</svelte:head>
 
 <div>
     <h1>Shop</h1>
